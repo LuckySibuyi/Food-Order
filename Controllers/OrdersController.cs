f50429102132e14caee014b7f9ae0a0a4fe8dd86
LuckySibuyi <lucky1sibuyi@gmail.com>
2025-06-19 08:16:33 +0200

    Http get method

---
+++ Controllers/OrdersController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using FoodOrderApi.Models;
+
+namespace FoodOrderApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+       
+        static private readonly List<Order> orders = new()
+        {
+            new() { OrderId = 1, OrderName = "Pizza", OrderDate = "2023-10-01", OrderStatus = "Delivered" },
+            new() { OrderId = 2, OrderName = "Burger", OrderDate = "2023-10-02", OrderStatus = "Pending" }
+        };
+
+        [HttpGet]
+        public ActionResult<List<Order>> GetOrders()
+        {
+            return Ok(orders);
+        }
+    }
+   
+}
