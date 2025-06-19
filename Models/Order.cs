da514ce536a0d66940467131e07b70ff27a0c4ea
LuckySibuyi <lucky1sibuyi@gmail.com>
2025-06-19 07:39:42 +0200

    creating orders model
    Add Order class to FoodOrderApi.Models namespace
    
    Created a new namespace  and defined
    the  class with properties: , ,
    , and , initializing string properties
    to empty strings.

---
+++ Models/Order.cs
@@ -0,0 +1,11 @@
+﻿namespace FoodOrderApi.Models
+{
+    public class Order
+    {
+       public int OrderId { get; set; }
+        public string OrderName { get; set; } = string.Empty;
+        public string OrderDate { get; set; } = string.Empty;
+        public string OrderStatus { get; set; } = string.Empty;
+
+    }
+}
