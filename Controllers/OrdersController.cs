a01abded3d76c4fca4a6ad737ade6fdc99c7d13e
LuckySibuyi <lucky1sibuyi@gmail.com>
2025-06-19 07:43:30 +0200

    creating controller

---
+++ Controllers/OrdersController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FoodOrderApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+    }
+}
